ce295afd6be461d455d78e0292dac2e7f7f586d9
forrestben <134863467+forrestben@users.noreply.github.com>
2023-05-30 15:47:34 -0400

    Update maxconversion.js

--- public/maxconversion.js
+++ public/maxconversion.js
@@ -1,18 +1,23 @@
-// Your maxconversion.js code for browser execution
-// Modify the code as per your requirements
+// Add an event listener to the iframe load event
+iframe.addEventListener('load', function() {
+  // Access the iframe's content window
+  const iframeWindow = iframe.contentWindow;
 
-// Example: Accessing the DOM
-document.addEventListener('DOMContentLoaded', () => {
-  // Your code here for manipulating the DOM or interacting with elements
+  // Add an event listener to the iframe's document
+  iframeWindow.document.addEventListener('click', function(event) {
+    // Get the target element that was clicked
+    const targetElement = event.target;
+
+    // Perform your desired actions or edits on the target element
+    // For example, you can change its style or content
+    targetElement.style.backgroundColor = 'red';
+    targetElement.textContent = 'Edited Element';
+
+    // Send the edited element back to your server for processing or storage
+    // You can use AJAX or other methods to send the data
+
+    // Prevent the default behavior of the click event
+    event.preventDefault();
+    event.stopPropagation();
+  });
 });
-
-// Example: Making an AJAX request
-const xhr = new XMLHttpRequest();
-xhr.open('GET', '/api/data', true);
-xhr.onload = () => {
-  if (xhr.status === 200) {
-    const response = JSON.parse(xhr.responseText);
-    // Your code here for handling the response
-  }
-};
-xhr.send();
