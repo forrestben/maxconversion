6fe3443d27f64124a6bf6a8ec2bb59fed3afe9e4
forrestben <134863467+forrestben@users.noreply.github.com>
2023-05-30 01:52:24 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,43 +1,15 @@
 const express = require('express');
+const app = express();
 const path = require('path');
 
-const app = express();
+// Enable CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // Replace * with the specific domain if needed
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.use(express.json());
-
-let tests = [];
-
-// Create a new test
-app.post('/tests', (req, res) => {
-  const test = req.body;
-  tests.push(test);
-  res.status(201).send();
-});
-
-// Get a list of all tests
-app.get('/tests', (req, res) => {
-  res.json(tests);
-});
-
-// Update a test
-app.put('/tests/:id', (req, res) => {
-  const id = req.params.id;
-  const updatedTest = req.body;
-
-  let wasUpdated = false;
-  tests = tests.map(test => {
-    if (test.id === id) {
-      wasUpdated = true;
-      test = updatedTest;
-    }
-    return test;
-  });
-
-  if (wasUpdated) {
-    res.send();
-  } else {
-    res.status(404).send();
-  }
-});
 
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
@@ -45,6 +17,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Route handler for the root URL ("/")
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Set the MIME type for JavaScript files
+app.get('/maxconversion.js', (req, res) => {
+  res.setHeader('Content-Type', 'application/javascript');
+  res.sendFile(path.join(__dirname, 'public', 'maxconversion.js'));
 });
 
 const port = process.env.PORT || 3000;
