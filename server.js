d8dd343cb56d8fd02de0a6f7aa30f019310c7dd3
forrestben <134863467+forrestben@users.noreply.github.com>
2023-05-30 00:40:59 -0400

    Update server.js

--- server.js
+++ server.js
@@ -39,9 +39,6 @@ app.put('/tests/:id', (req, res) => {
   }
 });
 
-// Serve the static files from the "public" directory
-app.use(express.static('public'));
-
 // Route handler for the root URL ("/")
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
